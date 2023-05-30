05b665a3e3ad7cacdf95d94afb62522c00ef518a
ZakinBacon <zakin68@gmail.com>
2023-05-30 10:24:31 -0700

    Dictionary Comprehension and looping through pandas

--- main.py
+++ main.py
@@ -17,17 +17,68 @@
 
 # 26.3
 # Opens 2 files containing numbers and find the common things between the 2 lists.
-file1 = open("file1.txt", "r")
-new_file1 = [int(file) for file in file1]
+# file1 = open("file1.txt", "r")
+# new_file1 = [int(file) for file in file1]
+#
+# file2 = open("file2.txt", "r")
+# new_file2 = [int(file) for file in file2]
+#
+# s = set(new_file2)
+#
+# result = [file for file in new_file1 if file in s]
+#
+# # Write your code above 👆
+#
+# print(result)
 
-file2 = open("file2.txt", "r")
-new_file2 = [int(file) for file in file2]
+# 26.4 Uses dictionary comprehension to create a dictionary of all the words in the string and then give the length
+# of the word in the dic.
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+#
+# words = sentence.split()
+#
+# result = {word:len(word) for word in words}
+#
+# print(result)
 
-s = set(new_file2)
 
-result = [file for file in new_file1 if file in s]
+# 26.5 Uses dictionary comprehension to go over a dictionary and change the temps from C to F
 
-# Write your code above 👆
+# weather_c = {
+#     "Monday": 12,
+#     "Tuesday": 14,
+#     "Wednesday": 15,
+#     "Thursday": 14,
+#     "Friday": 21,
+#     "Saturday": 22,
+#     "Sunday": 24,
+# }
+#
+# weather_f = {day:((temp * 9/5) + 32) for (day,temp) in weather_c.items()}
+#
+# print(weather_f)
 
-print(result)
+# 26.6 Looping through Pandas
 
+# import pandas
+# student_dict = {
+#     "student": ["angela", "james", "lily"],
+#     "score": [56, 76, 98]
+# }
+#
+# student_data_frame = pandas.DataFrame(student_dict)
+# print(student_data_frame)
+
+# # loop through a data frame
+#
+# for (key, value) in student_data_frame.items():
+#     print(value)
+
+# loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     print(row.score)
+
+# # Loop through row with a specific row in mind
+# for (index, row) in student_data_frame.iterrows():
+#     if row.student == "angela":
+#         print(row.score)
